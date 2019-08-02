bcf7cba8e255798a67c721ff970530e4923d0cf3
amir <amirgiraffe@gmail.com>
2019-08-02 17:15:57 +0300

    add cycle index to the animation object

--- include/animations/i_animation.h
+++ include/animations/i_animation.h
@@ -16,15 +16,30 @@ public:
 protected:
   virtual void Render(float rel_time) = 0;
 
+  // animation can be notified via this virtual method on a new cycle that starts.
+  // each derived class can choose to implement this function if it likes, or just ignore the event
+  virtual void NewCycle(int cycleIndex) {}
+
 public:
 
   void Render(unsigned long curr_time) {
+
     float rel_time = ((float)(curr_time - start_time) / (float)(end_time - start_time));  
     if(this->repeatNum > 0.0f) { // has repeats - change relTime
-      rel_time = fmod(rel_time * this->repeatNum, 1.0f);
+
+      float cycleTime = rel_time * this->repeatNum;
+      rel_time = fmod(cycleTime, 1.0f);
+      int currCycleIndex = floor(cycleTime);
+
+      if(currCycleIndex != this->lastCycleIndex) {
+        this->lastCycleIndex = currCycleIndex;
+        NewCycle(currCycleIndex);
+      }
+
       // check if a "repeat" function should be rendered for this time
       if(rel_time < this->repeatStart || rel_time > this->repeatEnd)
         return;
+
     }
     Render(rel_time);
   }
@@ -46,6 +61,8 @@ protected:
   unsigned long start_time, end_time;
   float repeatNum, repeatStart, repeatEnd;
   const std::vector<HSV *> *pixels;
+
+  int lastCycleIndex = -1;
 };
 
 #endif // __I_ANIMATIONS_H__
